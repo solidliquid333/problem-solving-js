007652dca6c938db3bfbd317e129a26499a14c5b
khalid Tuhin <76039658+khalid-Ibne-Hussain@users.noreply.github.com>
2023-10-26 03:08:15 +0600

    Update factorialRecursion.js

--- factorialRecursion.js
+++ factorialRecursion.js
@@ -1,1 +1,35 @@
 
+const RecursionFactorial = (num) => {
+
+    function factorial(num) {
+        if (num === 0 || num === 1) {
+            return 1;
+        }
+        else {
+            let value = num * factorial(num - 1);
+            return value;
+        }
+    }
+    console.log(factorial(num));
+
+    // add token with the value
+
+    const token = "xyz123";
+    const value = factorial(num).toString();
+
+    // concat token with the value
+    const valueWithToken = value + token;
+
+    // concat and then replace every 2nd char with _
+    let value2Array = valueWithToken.split("");
+    for (let i = 2; i < valueWithToken.length; i += 3) {
+        value2Array[i] = "_";
+    }
+
+    return value2Array.join("");
+
+
+}
+
+const num = 8;
+console.log(RecursionFactorial(num));
